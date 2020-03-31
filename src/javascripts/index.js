d5d7bdf55c5cba8d254872133828914fb18cd1bb
I shot myself again <hannahtaylor1@mail.weber.edu>
2020-03-31 15:19:58 -0600

    Help

--- src/javascripts/index.js
+++ src/javascripts/index.js
@@ -5,3 +5,24 @@ require.context('../stylesheets/', true, /\.(css|scss)$/i)
 
 // JavaScript
 //TODO
+import { movies } from './movies'
+
+let featured_movie = document.quarySelector('.featured')
+for(let m of movies){
+    let movie_thumb = document.getElementById('m' + m.id)
+    movie_thumb.innerHTML = `
+    <img src="${m.poster}">
+    `
+    
+    movie_thumb.onclick = function(){
+        selectMovie(m)
+    }
+}
+
+function selectMovie(m){
+    featured_movie.innerHTML = `
+    <img src="${m.poster}" style="float: left;">
+    <h1>${m.title}</h1>
+    <p>${m.plot}</p>
+    `
+}
